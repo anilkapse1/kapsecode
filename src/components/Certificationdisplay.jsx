c0164ecedf1f947de02460a6e7cca628fb62a21b
anil <anil.k@orioninc.com>
2023-03-24 20:32:47 +0530

    image path changed

--- src/components/Certificationdisplay.jsx
+++ src/components/Certificationdisplay.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
+
 const Certificationdisplay = ({id,img}) => {
   return (
     <>
         <span>{id}</span>
         <div className='certification_image'>
-        <img src={`/src/assets/images/certification/${img}.png`} alt="mern"/>
+        <img src={`/kapsecode/src/assets/images/certification/${img}.png`} alt="mern"/>
         </div>
   </>
 
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,17 +3,17 @@ import {  Outlet } from 'react-router-dom'
 import styled from 'styled-components'
 import Header from '../components/Header'
 import Container from '../Container'
-import plush from '../assets/images/plus.png'
+import plus from '../assets/images/plus.png'
 
 
 const Portfolio = () => {
     const Portfolio=styled.div`
-        ${'' /* background: linear-gradient(184deg, rgba(170,174,205,1) 0%, rgba(255,255,255,1) 57%); */}
+        border:1px solid red;
         min-height:100vh;
         position:relative;
         z-index:999;
         &::after{
-            background: url(/src/assets/images/plus.png) right top / 50% repeat-y;
+            background: url(${plus}) right top / 50% repeat-y;
             content: "";
             position: absolute;
             width: 100%;
