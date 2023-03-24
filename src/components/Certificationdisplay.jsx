a271197564ce6d0c05dbc9c6af24709e83fcf6ed
anil <anil.k@orioninc.com>
2023-03-24 20:41:20 +0530

    image paht changed new

--- src/components/Certificationdisplay.jsx
+++ src/components/Certificationdisplay.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-
+import mern from '../assets/images/certification/mern.png'
 
 const Certificationdisplay = ({id,img}) => {
   return (
     <>
         <span>{id}</span>
         <div className='certification_image'>
-        <img src={`/kapsecode/src/assets/images/certification/${img}.png`} alt="mern"/>
+        {/* <img src={`/kapsecode/src/assets/images/certification/${img}.png`} alt="mern"/> */}
+        <img src={mern} alt="mern"/>
         </div>
   </>
 
