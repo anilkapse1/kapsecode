81c6c140ae2bb744c00dd3af569d072b0182fb2b
anil <anil.k@orioninc.com>
2023-03-26 12:38:20 +0530

    media query added

--- src/Container.jsx
+++ src/Container.jsx
@@ -5,7 +5,16 @@ const Container = (props) => {
     const Mycontainer=styled.div`
         max-width:1084px;
         margin:auto;
+        @media (min-width: 1300px) {
+          max-width: 1300px;
+          margin:auto;
+        }
+
+
+
     `
+
+
   return (
     <Mycontainer className='custom_container'>
         {props.children}
